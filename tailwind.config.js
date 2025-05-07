f15d9da2f7c69eb29edafe580c42f9c838ff0e81
V0153 <Karakurt58@windowslive.com>
2025-05-07 21:42:59 +0300

    Add Angular Material and update styles; implement login and register components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-orange': '#FFEDD5', // açık turuncu rengi ekledim
+        'dark-orange': '#F97316', // koyu turuncu rengi ekledim
+      },
+      animation: {
+        marquee: 'marquee 10s linear infinite', // Marquee animasyonu
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(100%)' }, // sağdan sola hareket
+          '100%': { transform: 'translateX(-100%)' }, // sola doğru hareket
+        },
+      },
+    },
   },
   plugins: [],
 }
-
